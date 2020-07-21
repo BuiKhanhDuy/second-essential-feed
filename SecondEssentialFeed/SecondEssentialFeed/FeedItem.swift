703b059be094822ba680aec145cd76e10d46f4ee
Duy Bui <duy.bui@asnet.com.vn>
2020-07-21 10:04:09 +0700

    Update structure codebase

---
+++ SecondEssentialFeed/SecondEssentialFeed/FeedItem.swift
@@ -0,0 +1,18 @@
+//
+//  FeedItem.swift
+//  SecondEssentialFeed
+//
+//  Created by Macbook on 7/21/20.
+//  Copyright © 2020 Duy Bui. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
+
+
